3d340b60c081043d2d577a991c9a1d2ebffe7ce6
emanelkafrawy <emanelkafrawy2018@gmail.com>
2020-08-12 23:07:11 +0200

    mongoose part3

--- models/dishes.js
+++ models/dishes.js
@@ -1,16 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min:1,
+        max:5,
+        required:true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    auther: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
 const dishSchema = new Schema({
     name: {
         type: String,
-        require: true,
+        required : true,
         unique: true
     },
     description: {
         type: String,
-        require: true
-    }
+        required: true
+    },
+    comments: [commentSchema] //subdocument inside  dishdocument  +array
 },{
     timestamps: true
 });
