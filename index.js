d70e0e9057f3f7c1dd00234e7b6314d8987dfb48
emanelkafrawy <emanelkafrawy2018@gmail.com>
2020-08-12 21:05:20 +0200

    mongoose part2

--- index.js
+++ index.js
@@ -8,26 +8,24 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log("connected correctly to server");
 
-    var newDish = Dishes({
+        Dishes.create ({ //this way create object and save changes
         name:'eman',
         description:'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+        
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
-            
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 });
